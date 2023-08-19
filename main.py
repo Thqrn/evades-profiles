c70198fafee729369cf3c29140449138424075fd
Frosty7473 <frostlord7473@gmail.com>
2023-08-18 20:48:35 -0400

    searching through users who have stuff

--- main.py
+++ main.py
@@ -62,163 +62,217 @@ hatcounts = {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "silver-crown": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "bronze-crown": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "witch-hat": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "stars": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "santa-hat": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "blue-santa-hat": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "flames": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "blue-flames": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "halo": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "gold-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "spring-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "autumn-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "winter-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "summer-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "summer-olympics-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "summer-olympics-wreath-2": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "winter-olympics-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "sunglasses": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "flower-headband": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "autumn-leaves": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "fedora": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "coconut-holes": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "plastic-shine": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "pellets": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "pirate-hat": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "rose-wreath": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
 }
 bodycounts = {
@@ -226,112 +280,244 @@ bodycounts = {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "toxic-coat": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "orbit-ring": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "clouds": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "storm-clouds": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "tuxedo": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "angel-wings": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "one-winged-angel": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "stick": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "royal-robes": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "mummy-wrap": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     },
     "doughnut": {
         "unlockedCount":0,
         "usedCount":0,
         "unlockedVP":0,
-        "usedVP":0
+        "usedVP":0,
+        "unlockedUsers": [],
+        "usedUsers": []
     }
 }
 achievementcounts = {
-    "scientist": 0,
-    "tree_tapper": 0,
-    "snowball_mayhem": 0,
-    "savior": 0,
-    "money_moves": 0,
-    "pellet_obfuscator": 0,
-    "champion": 0,
-    "core_revival": 0,
-    "peculiar_pathing": 0,
-    "ground_control": 0,
-    "lush_sands": 0,
-    "baneful_bunker": 0,
-    "wacky_ride": 0,
-    "radioactive_dasher": 0,
-    "serene_garden": 0,
-    "stomper": 0,
-    "enormous_tribute": 0,
-    "glacial_grouping": 0,
-    "magnetic_highway": 0,
-    "gardener": 0,
-    "survivalist": 0,
-    "mysterious_mangrove": 0,
-    "ominous_occult": 0,
-    "relentless_ridge": 0,
-    "intense_inferno": 0,
-    "district_demolition": 0,
-    "two_winged_angel": 0,
-    "humongous_visit": 0,
-    "quintessential_quarry": 0,
-    "endless_expansion": 0,
-    "visit_grandma": 0,
-    "traveller": 0,
-    "victor": 0,
-    "one_winged_angel": 0,
-    "frosty_fjord": 0,
-    "dipole_diversion": 0
+    "scientist": {
+        "count":0,
+        "users":[]
+    },
+    "tree_tapper": {
+        "count":0,
+        "users":[]
+    },
+    "snowball_mayhem": {
+        "count":0,
+        "users":[]
+    },
+    "savior": {
+        "count":0,
+        "users":[]
+    },
+    "money_moves": {
+        "count":0,
+        "users":[]
+    },
+    "pellet_obfuscator": {
+        "count":0,
+        "users":[]
+    },
+    "champion": {
+        "count":0,
+        "users":[]
+    },
+    "core_revival": {
+        "count":0,
+        "users":[]
+    },
+    "peculiar_pathing": {
+        "count":0,
+        "users":[]
+    },
+    "ground_control": {
+        "count":0,
+        "users":[]
+    },
+    "lush_sands": {
+        "count":0,
+        "users":[]
+    },
+    "baneful_bunker": {
+        "count":0,
+        "users":[]
+    },
+    "wacky_ride": {
+        "count":0,
+        "users":[]
+    },
+    "radioactive_dasher": {
+        "count":0,
+        "users":[]
+    },
+    "serene_garden": {
+        "count":0,
+        "users":[]
+    },
+    "stomper": {
+        "count":0,
+        "users":[]
+    },
+    "enormous_tribute": {
+        "count":0,
+        "users":[]
+    },
+    "glacial_grouping": {
+        "count":0,
+        "users":[]
+    },
+    "magnetic_highway": {
+        "count":0,
+        "users":[]
+    },
+    "gardener": {
+        "count":0,
+        "users":[]
+    },
+    "survivalist": {
+        "count":0,
+        "users":[]
+    },
+    "mysterious_mangrove": {
+        "count":0,
+        "users":[]
+    },
+    "ominous_occult": {
+        "count":0,
+        "users":[]
+    },
+    "relentless_ridge": {
+        "count":0,
+        "users":[]
+    },
+    "intense_inferno": {
+        "count":0,
+        "users":[]
+    },
+    "district_demolition": {
+        "count":0,
+        "users":[]
+    },
+    "two_winged_angel": {
+        "count":0,
+        "users":[]
+    },
+    "humongous_visit": {
+        "count":0,
+        "users":[]
+    },
+    "quintessential_quarry": {
+        "count":0,
+        "users":[]
+    },
+    "endless_expansion": {
+        "count":0,
+        "users":[]
+    },
+    "visit_grandma": {
+        "count":0,
+        "users":[]
+    },
+    "traveller": {
+        "count":0,
+        "users":[]
+    },
+    "victor": {
+        "count":0,
+        "users":[]
+    },
+    "one_winged_angel": {
+        "count":0,
+        "users":[]
+    },
+    "frosty_fjord": {
+        "count":0,
+        "users":[]
+    },
+    "dipole_diversion": {
+        "count":0,
+        "users":[]
+    }
 }
 
 hat = list(hatcounts.keys())
@@ -366,9 +552,11 @@ for i, userprofile in enumerate(profiles):
                 print(f"{username} has {h}")
             userinfo.cosmetics+=1
             userinfo.hats+=1
+            hatcounts[h]["unlockedUsers"].append(username)
             hatcounts[h]["unlockedCount"]+=1
             hatcounts[h]["unlockedVP"]+=userdict["stats"]["highest_area_achieved_counter"]
         if h == userdict["accessories"]["hat_selection"]:
+            hatcounts[h]["usedUsers"].append(username)
             hatcounts[h]["usedCount"]+=1
             hatcounts[h]["usedVP"]+=userdict["stats"]["highest_area_achieved_counter"]
     for b in body:
@@ -377,14 +565,17 @@ for i, userprofile in enumerate(profiles):
                 print(f"{username} has {b}")
             userinfo.cosmetics+=1
             userinfo.bodies+=1
+            bodycounts[b]["unlockedUsers"].append(username)
             bodycounts[b]["unlockedCount"]+=1
             bodycounts[b]["unlockedVP"]+=userdict["stats"]["highest_area_achieved_counter"]
         if b == userdict["accessories"]["body_selection"]:
+            bodycounts[b]["usedUsers"].append(username)
             bodycounts[b]["usedCount"]+=1
             bodycounts[b]["usedVP"]+=userdict["stats"]["highest_area_achieved_counter"]
     for a in achievement:
         if a in userdict["stats"]["achievements"]:
-            achievementcounts[a]+=1
+            achievementcounts[a]["users"].append(username)
+            achievementcounts[a]['count']+=1
     userlist.append(userinfo)
 
 print(f"Checked {len(profiles)} profiles in {time.perf_counter()-start:.2f} seconds.")
@@ -399,12 +590,14 @@ print(f"\nBody cosmetic statistics from {len(profiles)} users:")
 processStats(bodycounts, "body cosmetic")
 
 print(f"\nAchievement statistics from {len(profiles)} users:")
-achievementcounts = dict(sorted(achievementcounts.items(), key=lambda item: item[1]))
-
+achievementcounts = dict(sorted(achievementcounts.items(), key=lambda item: item[1]['count']))
 percents = []
+countsachievements = []
 for a in achievementcounts:
-    percents.append(f"{achievementcounts[a]/len(profiles)*100:.2f}%")
-twoarray = [percents, list(achievementcounts.values()), list(achievementcounts.keys())]
+    percents.append(f"{achievementcounts[a]['count']/len(profiles)*100:.2f}%")
+    countsachievements.append(achievementcounts[a]['count'])
+print(countsachievements)
+twoarray = [percents, countsachievements, list(achievementcounts.keys())]
 tablizer(list(zip(*twoarray[::-1])), header=["Achievement", "Count", "Percent"], alignment=["left", "right"])
 
 hatcounts["usersChecked"] = len(profiles)
@@ -446,9 +639,37 @@ with open('bodieslb.txt', 'w', encoding='utf-8') as f:
     for u in userlist:
         f.write(f"{u.name}: {u.bodies}\n")
 
-
-
 print("Saved to files.")
+
+while True:
+    # try:
+    print("Enter a cosmetic or achievement to view its speciric users or those who have it unlocked, or 'exit' to exit:")
+    print("NOTE: Values are not ordered in any specific order.")
+    search = input("")
+    if search == "exit":
+        break
+    elif search in hat or search in body:
+        usedorunlocked = input("Enter 'used' or 'unlocked' to view the users who have used or unlocked the cosmetic:\n")
+        if usedorunlocked == "used":
+            if search in hat:
+                for i, u in enumerate(hatcounts[search]["usedUsers"]):
+                    print(f"{i+1}. {u}")
+            elif search in body:
+                for i, u in enumerate(bodycounts[search]["usedUsers"]):
+                    print(f"{i+1}. {u}")
+        elif usedorunlocked == "unlocked":
+            if search in hat:
+                for i, u in enumerate(hatcounts[search]["unlockedUsers"]):
+                    print(f"{i+1}. {u}")
+            elif search in body:
+                for i, u in enumerate(bodycounts[search]["unlockedUsers"]):
+                    print(f"{i+1}. {u}")
+    elif search in achievement:
+        for i, u in enumerate(achievementcounts[search]["users"]):
+            print(f"{i+1}. {u}")
+    else:
+        print("Invalid input")
+
 
 print()
 print("Done")
